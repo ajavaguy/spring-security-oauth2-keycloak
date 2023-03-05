814477088157a295ab1ef6711f853301b101edea
ajavaguy <ajavaguy96@gmail.com>
2023-03-05 22:54:55 +0700

    Init project

---
+++ oauth2-pkce/src/test/java/com/example/oauth2pkce/Oauth2PkceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2pkce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PkceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-security-oauth2-keycloak-demo/src/test/java/com/example/springsecurityoauth2keycloakdemo/SpringSecurityOauth2KeycloakDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityoauth2keycloakdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2KeycloakDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
